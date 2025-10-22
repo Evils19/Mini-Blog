a85dbc7d21c5db19fa307aa14b37da62b93df7e2
Evils19 <evelin.jitaru@iis.utm.md>
2025-10-22 19:25:25 +0300

    Add EF Core migrations for comments and post image support

---
+++ MyBlazorServerBlog/Migrations/20251020170359_Addimg.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyBlazorServerBlog.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addimg : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ImageData",
+                table: "Posts",
+                type: "BLOB",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageData",
+                table: "Posts");
+        }
+    }
+}
